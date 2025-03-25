17d22a8f30100b187bba53b100e44bfa7995c7c0
unsharot <U_N_Sharot@yahoo.co.jp>
2025-03-25 16:23:31 +0900

    パディングの色を消した

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,17 +10,17 @@
 	<title>地理クイズ - unsharot</title>
 </svelte:head>
 
-<header class="bg-teal-500 p-10 text-center text-black font-serif text-[5vw] pt-30">
+<header class="p-10 text-center text-black font-serif text-[5vw] pt-30">
 	<h1>地理クイズ</h1>
 </header>
 
 <main class="mx-auto flex min-h-dvh max-w-4xl flex-col gap-4 bg-white p-8">
-	<section class="rounded-lg bg-amber-300 px-4 py-8 font-serif">
+	<section class="rounded-lg px-4 py-8 font-serif">
 		<H2 text="問題" />
 		<p>太郎君は衛星データのラベルを紛失してしまいました。以下の衛星データの資料から、これらのデータはどの地点でとられたものか特定してください。(配点100点)</p>
 	</section>
 
-	<section class="flex flex-col gap-4 rounded-lg bg-amber-300 px-4 py-8 font-serif">
+	<section class="flex flex-col gap-4 rounded-lg  px-4 py-8 font-serif">
 		<H2 text="資料" />
 		<div class="grid grid-cols-3 gap-6">
 			<DataTile />
@@ -33,7 +33,7 @@
 		</div>
 	</section>
 
-	<section class="rounded-lg bg-blue-200 p-4">
+	<section class="rounded-lgp-4">
 		<H2 text="解答" />
 		<!-- TODO: ここに回答用の地図が入る -->
 		<!-- svelte-maplibre使うとよさそう -->
@@ -44,6 +44,6 @@
 	</section>
 </main>
 
-<footer class="bg-sky-500 p-4 text-center text-white">
+<footer class="bg-sky-500 p-1 text-center text-white text-sm font-serif">
 	<h1>資料はJAXA Earth APIを利用して作成したものです</h1>
 </footer>
