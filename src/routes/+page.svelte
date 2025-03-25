98d3e3408e082a40b5101cd1fb4336124f7b43b9
Sorami Hisamoto <hisamoto@mierune.co.jp>
2025-03-25 14:48:41 +0900

    WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
+<script lang="ts">
+	// ここにTypeScriptを書く
+</script>
+
+<!-- HTMLのhead要素はsvelte:headタグで書く -->
 <svelte:head>
 	<title>地理クイズ - unsharot</title>
 </svelte:head>
+
+<header class="bg-teal-500 p-4 text-center text-white">
+	<h1>ヘッダー</h1>
+</header>
+
+<main class="mx-auto min-h-dvh max-w-4xl bg-amber-100 p-8">メイン</main>
+
+<header class="bg-sky-500 p-4 text-center text-white">
+	<h1>フッター</h1>
+</header>
