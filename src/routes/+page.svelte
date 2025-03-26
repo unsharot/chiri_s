d42e88dbf8a5627e124177dd59bafd354faa5f74
unsharot <U_N_Sharot@yahoo.co.jp>
2025-03-26 10:29:29 +0900

    formatを適用

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,17 +10,19 @@
 	<title>地理クイズ - unsharot</title>
 </svelte:head>
 
-<header class="bg-yellow-50 p-1 text-center text-black font-serif text-5xl pt-15">
+<header class="bg-yellow-50 p-1 pt-15 text-center font-serif text-5xl text-black">
 	<h1>地理クイズ</h1>
 </header>
 
 <main class="mx-auto flex min-h-dvh max-w-4xl flex-col gap-4 bg-yellow-50 p-8">
 	<section class="rounded-lg px-4 py-8 font-serif">
 		<H2 text="問題" />
-		<p>太郎君は衛星データのラベルを紛失してしまいました。以下の衛星データの資料から、これらのデータはどの地点でとられたものか特定してください。(配点100点)</p>
+		<p>
+			太郎君は衛星データのラベルを紛失してしまいました。以下の衛星データの資料から、これらのデータはどの地点でとられたものか特定してください。(配点100点)
+		</p>
 	</section>
 
-	<section class="flex flex-col gap-4 rounded-lg  px-4 py-8 font-serif">
+	<section class="flex flex-col gap-4 rounded-lg px-4 py-8 font-serif">
 		<H2 text="資料" />
 		<div class="grid grid-cols-3 gap-6">
 			<DataTile />
@@ -29,7 +31,6 @@
 			<DataTile />
 			<DataTile />
 			<DataTile />
-
 		</div>
 	</section>
 
@@ -44,6 +45,6 @@
 	</section>
 </main>
 
-<footer class="bg-sky-500 p-1 text-center text-white text-sm font-serif">
+<footer class="bg-sky-500 p-1 text-center font-serif text-sm text-white">
 	<h1>資料はJAXA Earth APIを利用して作成したものです</h1>
 </footer>
