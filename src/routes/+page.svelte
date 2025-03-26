4be3c53df4e8c94525e65c02524a78258171a855
unsharot <U_N_Sharot@yahoo.co.jp>
2025-03-26 13:35:29 +0900

    JAXA APIの適用 仮

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,65 @@
 <script lang="ts">
 	import DataTile from './DataTile.svelte';
 	import { MapLibre } from 'svelte-maplibre-gl';
+	import * as je from './jaxa.earth.esm.js';
+
+	// const image = await je.getImage({
+	// 	//利用するデータセットのcollection.jsonのURLを記載します。
+	// 	collection:
+	// 		'https://s3.ap-northeast-1.wasabisys.com/je-pds/cog/v1/JAXA.EORC_ALOS.PRISM_AW3D30.v3.2_global/collection.json',
+
+	// 	//利用するデータセットのバンドを指定します。
+	// 	band: 'DSM',
+
+	// 	//データを取得する緯度経度範囲を記載します。
+	// 	//[最小経度(西), 最小緯度(南), 最大経度(東), 最大緯度(北)]
+	// 	// bbox: [-180, -90, 180, 90],
+	// 	// bbox: [11.39085, 47.27574, 17.0, 48.0],
+	// 	bbox: [16.0, 47.0, 17.0, 48.0],
+
+	// 	//データを取得する画像のサイズをピクセル数で記載します。
+	// 	// width: 1000,
+	// 	width: 500,
+	// 	height: 500,
+
+	// 	//可視化用のカラーマップを指定します。
+	// 	colorMap: {
+	// 		//最小値
+	// 		min: 0,
+	// 		//最大値
+	// 		max: 6000,
+	// 		//最小値～最大値をjet（青～緑～黄～赤の虹色）で塗りつぶします。
+	// 		colors: 'jet'
+	// 	}
+	// });
+
+	// これを取得したいです
+
+	// const image = await je.getImage({
+	// 	//利用するデータセットのcollection.jsonのURLを記載します。
+	// 	collection:
+	// 		'https://s3.ap-northeast-1.wasabisys.com/je-pds/cog/v1/JAXA.EORC_ALOS.PRISM_AW3D30.v3.2_global/collection.json',
+	// 	//利用するデータセットのバンドを指定します。
+	// 	band: 'DSM',
+	// 	//データを取得する緯度経度範囲を記載します。
+	// 	//[最小経度(西), 最小緯度(南), 最大経度(東), 最大緯度(北)]
+	// 	// bbox: [-180, -90, 180, 90],
+	// 	// bbox: [11.39085, 47.27574, 17.0, 48.0],
+	// 	bbox: [16.0, 47.0, 17.0, 48.0],
+	// 	//データを取得する画像のサイズをピクセル数で記載します。
+	// 	// width: 1000,
+	// 	width: 500,
+	// 	height: 500,
+	// 	//可視化用のカラーマップを指定します。
+	// 	colorMap: {
+	// 		//最小値
+	// 		min: 0,
+	// 		//最大値
+	// 		max: 6000,
+	// 		//最小値～最大値をjet（青～緑～黄～赤の虹色）で塗りつぶします。
+	// 		colors: 'jet'
+	// 	}
+	// });
 
 	// ここにTypeScriptを書く
 </script>
@@ -25,7 +84,23 @@
 	<section class="flex flex-col gap-4 rounded-lg px-4 py-8">
 		<h2 class="text-2xl font-bold">資料</h2>
 		<div class="grid grid-cols-3 gap-6">
-			<DataTile />
+			<div class="flex flex-col items-center justify-center gap-3 rounded bg-yellow-50 p-4">
+				<!-- {#await je.getImage({ //利用するデータセットのcollection.jsonのURLを記載します。
+					collection: 'https://s3.ap-northeast-1.wasabisys.com/je-pds/cog/v1/JAXA.EORC_ALOS.PRISM_AW3D30.v3.2_global/collection.json', band: 'DSM', // bbox: [-180, -90, 180, 90], //[最小経度(西), 最小緯度(南), 最大経度(東), 最大緯度(北)] //利用するデータセットのバンドを指定します。 //データを取得する緯度経度範囲を記載します。
+					// bbox: [11.39085, 47.27574, 17.0, 48.0],
+					bbox: [16.0, 47.0, 17.0, 48.0], width: 500, height: 500, //可視化用のカラーマップを指定します。 // width: 1000, //データを取得する画像のサイズをピクセル数で記載します。
+					colorMap: { //最小値
+						min: 0, max: 6000, colors: 'jet' } })}
+					//最大値 //最小値～最大値をjet（青～緑～黄～赤の虹色）で塗りつぶします。
+					<div class="h-24 w-24 bg-gray-600 text-center text-white">タイル画像placeholder</div>
+				{:then value}
+					<div class="h-24 w-24 bg-gray-600 text-center text-white">{value}</div>
+				{:catch error}
+					<div class="h-24 w-24 bg-gray-600 text-center text-white">{error.message}</div>
+				{/await} -->
+				<div class="text-sm">情報テキスト</div>
+			</div>
+
 			<DataTile />
 			<DataTile />
 			<DataTile />
