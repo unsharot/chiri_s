9a9a823bea70d9b0e17c1d31bd84c8b0af7d136c
unsharot <U_N_Sharot@yahoo.co.jp>
2025-03-27 14:04:12 +0900

    正誤判定を追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -79,6 +79,22 @@
 	onMount(async () => {
 		await getHintItemImages();
 	});
+
+	/////////////////////////////////////////////////
+
+	// 正誤処理
+
+	let correct = $state(false);
+	let showAnswer = $state(false);
+
+	function answer() {
+		showAnswer = true;
+		if (isSamePoint(selectedPoint, ansPoint)) {
+			correct = true;
+		} else {
+			correct = false;
+		}
+	}
 </script>
 
 <svelte:head>
@@ -147,12 +163,25 @@
 			</MapLibre>
 		</div>
 
-		<button class="mt-4 rounded bg-teal-500 px-4 py-2 text-white hover:bg-teal-600">回答する</button
+		<button
+			class="mt-4 rounded bg-teal-500 px-4 py-2 text-white hover:bg-teal-600"
+			onclick={() => {
+				answer();
+			}}>答える</button
 		>
 
 		<h4>答え: ({ansPoint.lng}, {ansPoint.lat})</h4>
 		<h4>選択中: ({selectedPoint.lng}, {selectedPoint.lat})</h4>
 		<h4>距離: {calcDistance(ansPoint, selectedPoint)}</h4>
+		{#if showAnswer}
+			<div>
+				{#if correct}
+					正解
+				{:else}
+					不正解
+				{/if}
+			</div>
+		{/if}
 	</section>
 </main>
 
