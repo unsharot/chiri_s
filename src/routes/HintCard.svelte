ff41fdb0c8b7ac7b04d1d915dffb2b58d55e4947
Sorami Hisamoto <hisamoto@mierune.co.jp>
2025-03-27 10:25:26 +0900

    Fix card bg style

--- src/routes/HintCard.svelte
+++ src/routes/HintCard.svelte
@@ -2,7 +2,7 @@
 	let { name, imgDataURL } = $props();
 </script>
 
-<div class="flex flex-col items-center justify-center gap-3 rounded bg-yellow-50 p-4">
+<div class="flex flex-col items-center justify-center gap-3 rounded p-4">
 	<img class="h-36 w-36 rounded-md border bg-white" src={imgDataURL} alt="{name}のタイル画像" />
 	<div class="font-bold">{name}</div>
 </div>
