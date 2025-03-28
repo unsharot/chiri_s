dd10664f8cf6572d55871bad80e0e914ea6815e9
unsharot <U_N_Sharot@yahoo.co.jp>
2025-03-28 11:13:50 +0900

    資料の名前を修正

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -84,7 +84,7 @@ type HintItem = {
 
 const initHintItems: HintItem[] = [
 	{
-		name: '標高',
+		name: '標高(数値表層モデル)',
 		api: {
 			collection:
 				'https://s3.ap-northeast-1.wasabisys.com/je-pds/cog/v1/JAXA.EORC_ALOS.PRISM_AW3D30.v3.2_global/collection.json',
@@ -96,7 +96,7 @@ const initHintItems: HintItem[] = [
 		imgDataURL: ''
 	},
 	{
-		name: '森林',
+		name: '森林非森林',
 		api: {
 			collection:
 				'https://s3.ap-northeast-1.wasabisys.com/je-pds/cog/v1/JAXA.EORC_ALOS-2.PALSAR-2_FNF.v2.1.0_global_yearly/collection.json',
@@ -108,7 +108,7 @@ const initHintItems: HintItem[] = [
 		imgDataURL: ''
 	},
 	{
-		name: '地表温度',
+		name: '地表面温度',
 		api: {
 			collection:
 				'https://s3.ap-northeast-1.wasabisys.com/je-pds/cog/v1/JAXA.G-Portal_GCOM-C.SGLI_standard.L3-LST.daytime.v3_global_monthly/collection.json',
@@ -120,7 +120,7 @@ const initHintItems: HintItem[] = [
 		imgDataURL: ''
 	},
 	{
-		name: '海面温度',
+		name: '海面水温',
 		api: {
 			collection:
 				'https://s3.ap-northeast-1.wasabisys.com/je-pds2/cog/v1/JAXA.G-Portal_GCOM-W.AMSR2_standard.L3-SST.daytime.v4_global_daily/collection.json',
