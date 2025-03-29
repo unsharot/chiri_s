0a5850f7da140b47025b1c23875ad48c95f85d0d
unsharot <U_N_Sharot@yahoo.co.jp>
2025-03-29 16:29:34 +0900

    正解・不正解の背景を大きく

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -110,9 +110,13 @@
 					<a class="underline hover:no-underline" href="https://github.com/unsharot">unsharot</a>
 					{#if quizMode === 'result'}
 						{#if correct}
-							<div class="absolute right-0 rounded bg-green-500 text-center text-4xl">正解</div>
+							<div class="absolute right-0 rounded bg-green-500 p-0.5 text-center text-4xl">
+								正解
+							</div>
 						{:else}
-							<div class="absolute right-0 rounded bg-red-500 text-center text-4xl">不正解</div>
+							<div class="absolute right-0 rounded bg-red-500 p-0.5 text-center text-4xl">
+								不正解
+							</div>
 						{/if}
 					{/if}
 				</div>
