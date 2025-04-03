98dc26e5734461a5be9509919eb9c59af0dbd686
unsharot <U_N_Sharot@yahoo.co.jp>
2025-04-04 01:19:16 +0900

    ヒントカードをクリックすると拡大表示する機能

--- src/routes/HintCard.svelte
+++ src/routes/HintCard.svelte
@@ -1,8 +1,40 @@
 <script lang="ts">
 	let { name, imgDataURL } = $props();
+
+	let open = $state(false);
+
+	function openLightBox() {
+		console.log('a');
+		open = true;
+	}
+	function closeLightBox() {
+		console.log('a');
+		open = false;
+	}
 </script>
 
-<div class="flex flex-col items-center justify-center gap-3 rounded p-4">
-	<img class="h-36 w-36 rounded-md border bg-white" src={imgDataURL} alt="{name}のタイル画像" />
-	<div class="font-bold">{name}</div>
-</div>
+{#if open}
+	<div class="absolute z-3 rounded shadow-gray-800">
+		<button onclick={closeLightBox}>
+			<img
+				class="h-180 w-180 rounded-md border bg-white"
+				src={imgDataURL}
+				alt="{name}のタイル画像"
+			/>
+		</button>
+		<div class="font-bold">{name}</div>
+	</div>
+	<div class="flex flex-col items-center justify-center gap-3 rounded p-4">
+		<button>
+			<img class="h-36 w-36 rounded-md border bg-white" src={imgDataURL} alt="{name}のタイル画像" />
+		</button>
+		<div class="font-bold">{name}</div>
+	</div>
+{:else}
+	<div class="flex flex-col items-center justify-center gap-3 rounded p-4">
+		<button onclick={openLightBox}>
+			<img class="h-36 w-36 rounded-md border bg-white" src={imgDataURL} alt="{name}のタイル画像" />
+		</button>
+		<div class="font-bold">{name}</div>
+	</div>
+{/if}
